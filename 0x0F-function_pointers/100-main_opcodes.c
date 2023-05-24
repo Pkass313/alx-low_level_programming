9dcdd0a0adcb22540baeafdfd724f4ce64744d3e
Patricia <pkass313@gmail.com>
2023-05-24 14:33:30 -0700

    100 main

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
-	
+
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
@@ -27,18 +27,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
-	
+
 	for (index = 0; index < bytes; index++)
 	{
 		opcode = *(unsigned char *)address;
 		printf("%.2x", opcode);
-		
+
 		if (index == bytes - 1)
 			continue;
 		printf(" ");
 
 		address++;
-	}		
+	}
 		printf("\n");
 
 		return (0);
